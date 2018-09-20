505642de6b78ff2396d55a703e78375fb6d88989
Laffreuxbob <olivier.bricaud@sfr.fr>
2018-09-20 18:08:48 +0200

    Front end get data

--- Todolist/Frontend/js/scriptTodo.js
+++ Todolist/Frontend/js/scriptTodo.js
@@ -1,15 +1,36 @@
 
-function display(){
-    fetch('http://127.0.0.1:8080/todos',{method:'get'})
-    .then(response => {
-        return (console.log(response.json()));
-      })
-      .then(json => {
-        console.log(json);
-        return json;
-      }).catch(err => {
-        console.log('Error occured with fetching ressources : ' + err)
-      });
+
+
+function getTodos(){
+    fetch('http://127.0.0.1:8080/todos/')
+    .then(response =>  response.json())
+    .then(data => {console.log(data);})
+    .then(data => data)
+    .catch(err => {
+      console.log('Error occured with fetching ressources : ' + err)
+    });
 }
 
-display();
+getTodos();
+
+
+
+
+// fetch('./api/some.json')
+//   .then(
+//     function(response) {
+//       if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//           response.status);
+//         return;
+//       }
+
+//       // Examine the text in the response
+//       response.json().then(function(data) {
+//         console.log(data);
+//       });
+//     }
+//   )
+//   .catch(function(err) {
+//     console.log('Fetch Error :-S', err);
+//   });
