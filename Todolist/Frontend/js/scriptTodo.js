2cb613923d7a192e12246f441e02020681aa4e95
Laffreuxbob <olivier.bricaud@sfr.fr>
2018-09-21 12:36:26 +0200

    front button add delete bootstrap

--- Todolist/Frontend/js/scriptTodo.js
+++ Todolist/Frontend/js/scriptTodo.js
@@ -1,4 +1,3 @@
-
 function getTodos(){
   fetch('http://127.0.0.1:8080/todos/', {method:'get'})
   .then(response =>  response.json())
@@ -11,22 +10,30 @@ function getTodos(){
 getTodos();
 
 function displayTodos(obj){
-  let main = document.getElementById("mainContent");
+  let main = document.getElementById("todoListDisplay");
   for(key in obj){
     
     let task = document.createElement('li');
     task.innerHTML = obj[key].name
-    task.className = "popup"
+    task.className = "popup list-group-item"
     
     let data = document.createElement('span')
-    data.innerHTML = "date : " + obj[key].date + "<br>" + "des : " + obj[key].description
+    data.innerHTML = "Date : " + obj[key].date + "<br>" + "Description : " + obj[key].description
     
     let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "delete";
+    deleteButton.innerHTML = "Delete";
     deleteButton.id = obj[key].name
+    deleteButton.className = "btn btn-danger";
     deleteButton.addEventListener('click', deleteTodo);
+
+    let editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    editButton.id = obj[key].name
+    editButton.className = "btn btn-warning";
+    editButton.addEventListener('click', editTodo);
     
     task.appendChild(data)
+    task.appendChild(editButton)
     task.appendChild(deleteButton)
     main.appendChild(task)
   }
@@ -44,19 +51,27 @@ function deleteTodo(){
   document.location.reload();
 }
 
-// (async () => {
-//   const rawResponse = await fetch('https://httpbin.org/post', {
-//     method: 'POST',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({a: 1, b: 'Textual content'})
-//   });
-//   const content = await rawResponse.json();
-
-//   console.log(content);
-// })();
+function editTodo(){
+  console.log(this.id)
+  let newName = document.getElementById("name").value;
+  let newDate = document.getElementById("date").value;
+  let newDescription = document.getElementById("description").value;
+  fetch('http://127.0.0.1:8080/todos/edit' + this.id, {
+    method:'put',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({"name": newName, "date": newDate, "description": newDescription})
+  })
+  .then(response =>  response.json())
+  .then(data => {console.log(data); displayTodos(data)})
+  .then(data => data)
+  .catch(err => {
+    console.log('Error occured with fetching ressources : ' + err)
+  });
+  document.location.reload();
+}
 
 function addTodo(){
   let newName = document.getElementById("name").value;
@@ -84,6 +99,38 @@ document.location.reload();
 function test(){
   console.log("test")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // function createGist(opts) {
